cd915e434711e68f7464bfcd48d79f4466bcc657
adamkoz1 <adamkoz11@gmail.com>
2019-08-25 15:12:23 +0200

    Updated test...

--- src/getMessage.test.js
+++ src/getMessage.test.js
@@ -4,7 +4,16 @@ describe('getMessage', () => {
     describe('for number equal 101', function() {
         it('returns "number is greater than 100"', function() {
             expect(getMessage(101)).toEqual('number is greater than 100'); //bylo abc - nie dzialalo...!!
+    describe('for number equal 101', function() {
+        it('number is NOT greater than 100"', function() {
+            expect(getMessage(50)).toEqual('number is NOT greater than 100');
 
         });
     });
 });
+
+
+TextDecoderStream('getMessage (version 2)', function() {
+    expect(getMessage(101).toEqual('number is greater than 100'));
+    expect(getMessage(50).toEqual('number is NOT greater than 100'));
+})
